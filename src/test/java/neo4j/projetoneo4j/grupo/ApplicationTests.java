a3f3cd825845e68ea703f7204c91463856ad1c90
Hebert Souza Raphalsky Do Nascimento <hebeyeemo@gmail.com>
2024-03-20 21:16:52 -0300

    Inicio-Projeto
    Commit-01

---
+++ src/test/java/neo4j/projetoneo4j/grupo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package neo4j.projetoneo4j.grupo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
